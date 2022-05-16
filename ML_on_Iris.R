e6b6595a2aea119048dd4741d1990239264f6d79
Rebecca N <46715599+RNowack@users.noreply.github.com>
2022-05-16 10:41:36 +0200

    Add files via upload

---
+++ ML_on_Iris.R
@@ -0,0 +1,71 @@
+
+# Standardcide
+setwd("C:/Users/Becci/Desktop/DataScience/Jupyter/Projects_2022")
+pacman::p_load(pacman, ggplot2, dplyr,tidyr, tidyverse, reticulate, rsample, caret)
+
+# https://bradleyboehmke.github.io/HOML/process.html 
+
+# Load and inspect data
+data(iris)
+view(iris)
+summary(iris)
+plot(iris)
+colnames(iris)
+
+x <- iris[,1:4]
+y <- iris[,5]
+
+featurePlot(x=x, y=y, plot="box")
+
+
+ggplot(iris)+ geom_point(aes(x = Petal.Length, y = Petal.Width), stroke = 2)+
+  facet_wrap(~ Species) +labs(x = "Petal Length", y = "Petal Width")+theme_bw()
+
+# Train test split with random sampling
+set.seed(123)  # for reproducibility
+split_1  <- initial_split(iris, prop = 0.7)
+train_1  <- training(split_1)
+test_1   <- testing(split_1)
+
+# Create partition
+train_partition = createDataPartition(iris$Species, list = FALSE)
+
+# Split data into train and test sets
+data_train = data[train_partition,]
+data_val = data[-train_partition,]
+
+# Run algorithms using 10-fold cross validation
+control <- trainControl(method="cv", number=10)
+metric <- "Accuracy"
+
+# The accuracy scores for the testing set can vary depending on what observations are in the set.
+# This disadvantage can be countered using k-fold cross-validation.
+# The accuracy score of the models depends on the observations in the testing set, which is determined
+# by the seed of the pseudo-random number generator (random_state parameter).
+# As a model's complexity increases, the training accuracy (accuracy you get when you train and test the model on the same data) increases.
+# If a model is too complex or not complex enough, the testing accuracy is lower.
+# For KNN models, the value of k determines the level of complexity. A lower value of k means that the model is more complex.
+
+predictions = knn(train, test, cl, params)
+
+
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=train_1, method="lda", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=train_1, method="knn", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=train_1, method="rf", metric=metric, trControl=control)
+
+# summarize accuracy of models
+results <- resamples(list(lda=fit.lda, knn=fit.knn, rf=fit.rf))
+summary(results)
+
+# summarize Best Model
+print(fit.lda)
+
+# estimate skill of LDA on the validation dataset
+predictions <- predict(fit.lda, train_1)
+confusionMatrix(predictions, train_1$Species)
